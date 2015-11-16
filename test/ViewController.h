4e73d9b3fde6a9cbef48696ab05a9cd08bbe9f11
Bizapper VM MacOS <vm@BizapperdeMac.local>
2015-11-15 18:14:07 -0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Bizapper VM MacOS  on 15/11/15.
+//  Copyright (c) 2015年 Bizapper VM MacOS . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by Bizapper VM MacOS  on 15/11/15.
+//  Copyright (c) 2015年 Bizapper VM MacOS . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
